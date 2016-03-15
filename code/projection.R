29f92f7d3674c572056718f7312b18eb3dcbf859
bjb40 <bryce.bartlett@duke.edu>
2016-03-15 14:38:31 -0400

    initial projections

--- code/projection.R
+++ code/projection.R
@@ -175,62 +175,121 @@ for(i in 1:1800){
 #Simulate future proportions
 #@@@@@@@@@@@@@@@@@@
 
-#Prepare Holder variables
+#function to simulate population change
+sim=function(p0){
+  #helper function for simulating population proportions
+  #p0 is an array of integers 15 age groups, 2 gener, and
+  #5 religious belonging
+  #returns 1800 simulated population distributions 6 years later
+    p1=array(0,c(iters,dim(p0)))
+    nm=list(1:iters);names(nm)='iter'; nm=c(nm,dimnames(p0))
+    dimnames(p1)=nm  
+    
+    #@@
+    #draw posterior predictives for 
+    #@@
+    
+    #cat('Beginning Simulation...\n')
+    #st = Sys.time()
+    
+    for(i in 1:iters){
+      if(i%%100==0){cat(i,'of',iters,'\n')}
+      
+      #check cohort component method -- may need to mean over interval ((x1+x2)/2)
+      #sumulate minor survival (assuming no changes)
+      p1[i,2,,] = mapply(p0[1,,],FUN=function(x) survive(x,sum(px.minors[1:3])))
+      p1[i,3,,] = mapply(p0[2,,],FUN=function(x) survive(x,sum(px.minors[4:6])))
+      p1[i,4,,] = mapply(p0[3,,],FUN=function(x) survive(x,sum(px.minors[7:9])))
+    
+      #simulate transitions for adult men and women
+      for(a in 5:14){
+       p1[i,a,1,] = trns(p0[a-1,1,],prob=phi[i,a-4,,])
+       p1[i,a,2,] = trns(p0[a-1,1,],prob=phi[i,a-4,,])
+      }
+    
+      #calculate final transition
+      p1[i,15,1,]=trns(p0[14,1,],prob=phi[i,11,,])+trns(p0[15,1,],prob=phi[i,12,,])
+      p1[i,15,2,]=trns(p0[14,1,],prob=phi[i,11,,])+trns(p0[15,1,],prob=phi[i,12,,])
+    
+      #simulate births
+      atrisk=p0[4:8,2,]
+      #pull values from iteration
+      fr=f[i,,]
+    
+      births=matrix(sapply(atrisk,FUN = function(x) sum(rbinom(x,size=1,prob=fr[(which(atrisk==x,arr.ind=TRUE))])),simplify='array'),5,5)
+      p1[i,1,1,] = apply(births,2,FUN=function(x) sum(rbinom(sum(x),size=1,prob=sexprop)))
+      p1[i,1,2,] = colSums(births)-p1[i,1,1,]
+    }
+    
+    return(p1)
+}
+
+#Prepare Holder variables, and simulate population change
 
 future = 5 #how many iterations
 p=list()
-  
-p1=array(0,c(iters,dim(p0)))
-nm=list(1:iters);names(nm)='iter'; nm=c(nm,dimnames(p0))
-dimnames(p1)=nm  
+p[[1]] = sim(p0=p0)
 
-#@@
-#draw posterior predictives for 
-#@@
-
-cat('Beginning Simulation...\n')
-
-for(i in 1:iters){
-  if(i%%100==0){cat(i,'of',iters,'\n')}
-  
-  #check cohort component method -- may need to mean over interval ((x1+x2)/2)
-  #sumulate minor survival (assuming no changes)
-  p1[i,2,,] = mapply(p0[1,,],FUN=function(x) survive(x,sum(px.minors[1:3])))
-  p1[i,3,,] = mapply(p0[2,,],FUN=function(x) survive(x,sum(px.minors[4:6])))
-  p1[i,4,,] = mapply(p0[3,,],FUN=function(x) survive(x,sum(px.minors[7:9])))
-
-  #simulate transitions for adult men and women
-  for(a in 5:14){
-   p1[i,a,1,] = trns(p0[a-1,1,],prob=phi[i,a-4,,])
-   p1[i,a,2,] = trns(p0[a-1,1,],prob=phi[i,a-4,,])
-  }
-
-  #calculate final transition
-  p1[i,15,1,]=trns(p0[14,1,],prob=phi[i,11,,])+trns(p0[15,1,],prob=phi[i,12,,])
-  p1[i,15,2,]=trns(p0[14,1,],prob=phi[i,11,,])+trns(p0[15,1,],prob=phi[i,12,,])
-
-  #simulate births
-  atrisk=p0[4:8,2,]
-  #pull values from iteration
-  fr=f[i,,]
-
-  births=matrix(sapply(atrisk,FUN = function(x) sum(rbinom(x,size=1,prob=fr[(which(atrisk==x,arr.ind=TRUE))])),simplify='array'),5,5)
-  p1[i,1,1,] = apply(births,2,FUN=function(x) sum(rbinom(sum(x),size=1,prob=sexprop)))
-  p1[i,1,2,] = colSums(births)-p1[i,1,1,]
+for(y in 2:future){
+  p0mean=apply(p[[y-1]],c(2,3,4),FUN=function(x) round(eff(x,c=.66)))[1,,,]
+  p[[y]]=sim(p0=p0mean)
 }
+
+#cat('One simulation took',Sys.time()-st)-- 
+#alittle over 30 seconds
 
 #@@@@@
 #output and figures
 #@@@@@
 
-props=array(0,c(1800,5))
-
-#proportion
-for(i in 1:iters){
-  props[i,] = apply(p1[i,,,],3,FUN=function(x) sum(x)/sum(p1[i,,,]))
+#calculate proportions
+props=list()
+for(y in 1:future){
+  props[[y]] = array(0,c(1800,5))
+  for(i in 1:iters){
+    props[[y]][i,] = apply(p[[y]][i,,,],3,FUN=function(x) sum(x)/sum(p[[y]][i,,,]))
+  }
 }
 
-par(mfrow=c(2,1))
-barplot(apply(p0,3,FUN=function(x) sum(x)/sum(p0)),col=colors1)
-barplot(apply(props,2,eff)[1,],col=colors1)
+bprop=apply(p0,3,FUN=function(x) sum(x)/sum(p0))
+base=rbind(bprop,bprop,bprop)
+plt=lapply(props,FUN=function(x) apply(x,2,eff))
+for(l in 1:5){plt[[(l+1)]]=plt[[l]]}
+plt[[1]]=base
+names(plt)=seq(2010,2010+(future*6),by=6)
+nm=c('Evangelical','Mainline','Other','Catholic','None')
+for(l in 1:6){colnames(plt[[l]])=nm}
+plotdat = list()
+for(l in 1:5){
+  plotdat[[l]] = sapply(1:6,FUN=function(x) plt[[x]][,l]) 
+}
+names(plotdat)=nm
 
+#line plot
+par(mfrow=c(1,1))
+plot(1,ylim=range(props),xlim=c(1,(future+1)),type='n',xaxt='n')
+  for(l in 1:6){
+    polygon(c(1:6,rev(1:6)),c(plotdat[[l]][2,],rev(plotdat[[l]][3,])),
+            border=NA,col=paste0(colors1[l],'45'))
+    
+  }
+
+  for(l in 1:6){
+     lines(1:6,plotdat[[l]][1,],col=colors1[l],lty=l)
+  }
+
+axis(1,labels=seq(2010,2010+(future*6),by=6),at=1:6)
+
+#barplots (18 years-- index no. 4)
+par(mfrow=c(1,5), mar=c(1,1,1,1), oma=c(1,1,1,1))
+yl = c(0,max(unlist(plotdat))+.05)
+mp=barplot(plotdat[[1]][1,c(1,4)],ylim=yl,col=colors1[1],xlab='Evangelical')
+  segments(mp[[2]],plotdat[[1]][2,4],mp[[2]],plotdat[[1]][3,4])
+  text(mp[[1]],0,'2010',pos=4)
+  mtext('Evangelical',side=1,cex=.75)
+  
+for(r in 2:5){  
+mp=barplot(plotdat[[r]][1,c(1,4)],ylim=yl,axes=F,col=colors1[r],xlab=nm[r])
+  segments(mp[[2]],plotdat[[r]][2,4],mp[[2]],plotdat[[r]][3,4])
+  mtext(nm[r],side=1,cex=.75)
+}
